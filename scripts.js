3bd1d731fe14608b9e678170b0f77834c30e0dc8
André Zopolato <andrezopo@gmail.com>
2022-04-08 00:32:07 -0300

    Versão desktop completa

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ let imagens = ["Imagens/explodyparrot.gif", "Imagens/fiestaparrot.gif", "Imagens
 let cartas = [];
 let numeroCartas = prompt(`Com quantas cartas gostaria de jogar?`);
 let contadorJogadas = 0;
+let travarVirada = false;
 
 while (4 > numeroCartas || numeroCartas > 14 || numeroCartas%2 !== 0){
 
@@ -14,6 +15,7 @@ function comparador(){
 
 function definirImagens(){
     imagens = imagens.sort(comparador);
+
     for (i = 0; i < numeroCartas/2; i++){
         cartas.push(imagens[i]);
         cartas.push(imagens[i]);
@@ -27,15 +29,54 @@ function quantidadeCartas(){
     let elemento = document.querySelector(".conteudo");
     for (i = 0; i < numeroCartas; i++){
         elemento.innerHTML += `
-            <div class="carta" onclick="virarCarta(this)">  
-                <img src="${cartas[i]}">        
+            <div class="carta" onclick="virarCartas(this)">  
+                <img class="carta${[i]}" src="${cartas[i]}">        
             </div>
         `}
     
 }
 quantidadeCartas()
 
-function virarCarta(elemento){
-    elemento.classList.add("virada")
-    contadorJogadas ++;
+function virarCartas(elemento){
+    
+    function desvirarCartas(){
+        document.querySelector(`.${posicao}`).parentNode.classList.remove("virada")
+        elemento.classList.remove("virada")
+        travarVirada = false;
+    }
+
+    
+    if (travarVirada){
+         return
+        }
+
+    if (elemento.classList.value.includes("virada")){
+
+    }
+    else{
+
+        elemento.classList.add("virada")
+        contadorJogadas ++;
+        if(document.querySelectorAll(".virada").length%2 !==0){
+            nomeImagem = elemento.querySelector("img").src
+            posicao = elemento.querySelector("img").classList.value
+        }
+        else if (nomeImagem === elemento.querySelector("img").src){
+
+
+        }else{
+            travarVirada = true;
+            setTimeout(desvirarCartas, 1000);
+            
+        }
+    
+    }
+    setTimeout(verificarVitoria, 100)
+    
+       
+}
+function verificarVitoria(){
+    if(document.querySelectorAll(".virada").length === cartas.length){
+        alert(`Você ganhou em ${contadorJogadas} jogadas!`)
+    }
 }
