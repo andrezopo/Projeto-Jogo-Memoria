acbea138eef947bd6a3d2cf26310a3663abbebd9
André Zopolato <andrezopo@gmail.com>
2022-04-09 02:20:36 -0300

    Relógio funcionando

--- scripts.js
+++ scripts.js
@@ -3,12 +3,15 @@ let cartas = [];
 let numeroCartas = prompt(`Com quantas cartas gostaria de jogar?`);
 let contadorJogadas = 0;
 let travarVirada = false;
+let minuto = 0;
+let segundo = 0;
 
 while (4 > numeroCartas || numeroCartas > 14 || numeroCartas%2 !== 0){
 
     numeroCartas = prompt(`Com quantas cartas gostaria de jogar?`);
 
 }
+
 function comparador(){
     return Math.random() - 0.5;
 }
@@ -78,6 +81,32 @@ function virarCartas(elemento){
 }
 function verificarVitoria(){
     if(document.querySelectorAll(".virada").length === cartas.length){
-        alert(`Você ganhou em ${contadorJogadas} jogadas!`)
+        clearInterval(idInterval);
+        alert(`Você ganhou em ${contadorJogadas} jogadas no tempo de ${minuto} minutos e ${segundo} segundos!`)
     }
 }
+function atualizarTimer(){
+    let elemento = document.querySelector(".timer")
+    if (segundo > 9 && minuto < 10){
+        elemento.innerHTML = `0${minuto.toPrecision(1)}:${segundo.toPrecision(2)}`
+    }
+    if (segundo < 10 && minuto < 10){
+        elemento.innerHTML = `0${minuto.toPrecision(1)}:0${segundo.toPrecision(1)}`
+    }
+    if (segundo < 10 && minuto > 9){
+        elemento.innerHTML = `${minuto.toPrecision(2)}:0${segundo.tofixed(0)}`
+    }
+    else{
+        elemento.innerHTML = `${minuto.toFixed(0)}:${segundo.tofixed(0)}`
+    }
+}
+function incrementarTempo(){
+    segundo ++
+    if (segundo === 60){
+        minuto ++;
+        segundo = 0;
+    }
+
+    atualizarTimer()
+}
+let idInterval = setInterval(incrementarTempo, 1000);
